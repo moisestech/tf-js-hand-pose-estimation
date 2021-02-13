bd55df02eb18622cbe08c1bd74ce3956cb550705
moisestech <m@moises.tech>
2021-02-13 15:40:13 -0500

    init react react-dom linkn to web app link reference

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import "./styles.css";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
 let project_name = "Tensorflow.js React Hand-Pose Estimation";
 
 console.log(project_name);
+
+// https://www.youtube.com/watch?v=f7uBsb-0sGQ
