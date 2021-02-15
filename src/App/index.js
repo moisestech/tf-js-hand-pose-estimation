31dae9ab768f4145fe08e6855678ef9fd19d7cc5
moisestech <m@moises.tech>
2021-02-15 08:44:19 -0500

    steps, react, useRef, imp drawHand, export App comp

---
+++ src/App/index.js
@@ -0,0 +1,29 @@
+// 1. Install dependencies
+// 2. Import dependencies
+// 3. Setup webcam and canvas
+// 4. Define references to those
+// 5. Load handpose
+// 6. Detect function
+// 7. Drawing utilities
+// 8. Draw functions
+
+import React, { useRef } from "react";
+
+// access to webcam
+import Webcam from "react-webcam";
+
+// for running object detection
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+
+// drawings x, y points on canvas
+import { drawHand } from "./utilities";
+
+export default function App() {
+  let project_name = "Tensorflow.js React Hand-Pose Estimation";
+
+  console.log(project_name);
+}
+
+// video: https://www.youtube.com/watch?v=f7uBsb-0sGQ
+// code: https://github.com/nicknochnack/HandPoseDetection
