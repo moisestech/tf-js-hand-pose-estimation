95a02e360a5714ef5678439233abdf760c591e35
moisestech <m@moises.tech>
2021-03-07 05:44:10 -0500

    detect function

--- src/App/index.js
+++ src/App/index.js
@@ -29,6 +29,37 @@ export default function App({project_name = "Tensorflow.js React Hand-Pose Estim
     console.log('Handpose model loaded!');
   }
 
+  // sets canvas and webcamfor drawing
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const hand = await net.estimateHands(video);
+      console.log(hand);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  };
+
   // inits model
   runHandpose();
 
