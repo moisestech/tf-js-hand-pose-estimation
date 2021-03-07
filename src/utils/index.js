be0a63e849b704da14787dfd61d7e3bd894ebbea
moisestech <m@moises.tech>
2021-03-07 05:32:21 -0500

    drawHand() predictions, ctx

--- src/utils/index.js
+++ src/utils/index.js
@@ -33,3 +33,55 @@ const style = {
   19: { color: "gold", size: 6 },
   20: { color: "gold", size: 6 },
 };
+
+// Drawing function
+export const drawHand = (predictions, ctx) => {
+  // Check if we have predictions
+  if (predictions.length > 0) {
+    // Loop through each prediction
+    predictions.forEach((prediction) => {
+      // Grab landmarks
+      const landmarks = prediction.landmarks;
+
+      // Loop through fingers
+      for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
+        let finger = Object.keys(fingerJoints)[j];
+        //  Loop through pairs of joints
+        for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
+          // Get pairs of joints
+          const firstJointIndex = fingerJoints[finger][k];
+          const secondJointIndex = fingerJoints[finger][k + 1];
+
+          // Draw path
+          ctx.beginPath();
+          ctx.moveTo(
+            landmarks[firstJointIndex][0],
+            landmarks[firstJointIndex][1]
+          );
+          ctx.lineTo(
+            landmarks[secondJointIndex][0],
+            landmarks[secondJointIndex][1]
+          );
+          ctx.strokeStyle = "plum";
+          ctx.lineWidth = 4;
+          ctx.stroke();
+        }
+      }
+
+      // Loop through landmarks and draw em
+      for (let i = 0; i < landmarks.length; i++) {
+        // Get x point
+        const x = landmarks[i][0];
+        // Get y point
+        const y = landmarks[i][1];
+        // Start drawing
+        ctx.beginPath();
+        ctx.arc(x, y, style[i]["size"], 0, 3 * Math.PI);
+
+        // Set line color
+        ctx.fillStyle = style[i]["color"];
+        ctx.fill();
+      }
+    });
+  }
+};
